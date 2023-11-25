13f6a21f25424022e353c6305bb3614d2235b559
kevin <kevinrochetaing46@gmail.com>
2023-11-25 20:55:50 +0100

    modification init pieces joueur

--- src/joueur/Joueur.java
+++ src/joueur/Joueur.java
@@ -19,21 +19,16 @@ public abstract class Joueur {
 
 	protected Joueur(Couleur couleur) {
 		this.couleur = couleur;
-		initPiece();
+		initPieces();
 	}
 
-	private void initPiece() {
-		if (couleur == Couleur.BLANC)
-			initPieceBlanc();
-		else
-			initPieceNoir();
-	}
+	private void initPieces() {
 
-	private void initPieceBlanc() {
-		int y = 0;
+		int y = couleur == Couleur.BLANC ? 0 : 7;
+		int yPion = couleur == Couleur.BLANC ? 1 : 6;
 
 		for (int x = 0; x < 8; x++)
-			listPiece.add(new Pion(couleur, new Coordonnees(x, y + 1)));
+			listPiece.add(new Pion(couleur, new Coordonnees(x, yPion)));
 
 		listPiece.add(new Tour(couleur, new Coordonnees(0, y)));
 		listPiece.add(new Tour(couleur, new Coordonnees(7, y)));
@@ -43,23 +38,6 @@ public abstract class Joueur {
 		listPiece.add(new Fou(couleur, new Coordonnees(5, y)));
 		listPiece.add(new Damme(couleur, new Coordonnees(3, y)));
 		listPiece.add(new Roi(couleur, new Coordonnees(4, y)));
-	}
-
-	private void initPieceNoir() {
-		int y = 7;
-
-		for (int x = 0; x < 8; x++)
-			listPiece.add(new Pion(couleur, new Coordonnees(x, y - 1)));
-
-		listPiece.add(new Tour(couleur, new Coordonnees(0, y)));
-		listPiece.add(new Tour(couleur, new Coordonnees(7, y)));
-		listPiece.add(new Cavalier(couleur, new Coordonnees(1, y)));
-		listPiece.add(new Cavalier(couleur, new Coordonnees(6, y)));
-		listPiece.add(new Fou(couleur, new Coordonnees(2, y)));
-		listPiece.add(new Fou(couleur, new Coordonnees(5, y)));
-		listPiece.add(new Damme(couleur, new Coordonnees(3, y)));
-		listPiece.add(new Roi(couleur, new Coordonnees(4, y)));
-
 	}
 
 	public List<Piece> getListPiece() {
